30d76603b93d50c4c6a5aaf1a96a41e0fe986f95
Yuji-Kakeya <rilakkumaimero@gmail.com>
2022-04-25 17:13:22 +0900

    first

--- src/main/java/local/kakeya/demo/model/Demo.java
+++ src/main/java/local/kakeya/demo/model/Demo.java
@@ -1,10 +1,16 @@
 package local.kakeya.demo.model;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class Demo {
 	public String message;
 
 	Demo(){
 		System.out.println("Constructor");
+		for (Method method: this.getClass().getMethods()){
+			System.out.println(method.getName());			
+		}
+		System.out.println(this.getClass().getModule().getClassLoader().resource);
 	}
 
 	public void setMessage(String message){
